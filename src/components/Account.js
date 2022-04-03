a4552260c3bbb3b489c131161db083de386f3559
Tudor Harries <tharries508@gmail.com>
2022-04-03 22:32:19 +0100

    Add basic sign up form

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,5 +1,54 @@
 function Account() {
-  return null;
+  const state = {};
+  const handleSubmit = () => {};
+  const handleChange = () => {};
+
+  return (
+    <>
+      <h1>Create account</h1>
+      <h3>Enter your details</h3>
+
+      <form onsubmit={handleSubmit}>
+        <label>
+          Given Name:
+          <input
+            type="text"
+            style={{ display: "block" }}
+            value={state.givenName}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Surname:
+          <input
+            type="text"
+            style={{ display: "block" }}
+            value={state.surname}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Email address:
+          <input
+            type="email"
+            style={{ display: "block" }}
+            value={state.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            style={{ display: "block" }}
+            value={state.password}
+            onChange={handleChange}
+          />
+        </label>
+        <input type="submit" value="Sign up" />
+      </form>
+    </>
+  );
 }
 
 export default Account;
